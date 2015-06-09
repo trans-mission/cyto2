62ce8f2cd1603d8e3b78e105d9e60a949a2beb36
mmansion <mikhail@tellart.com>
2015-06-09 18:48:59 -0400

    Simplify cyto.start() method, use utils for _rootAccessProps

--- cyto-lib/src/cyto/Cyto.js
+++ cyto-lib/src/cyto/Cyto.js
@@ -18,6 +18,7 @@ var cyto = function(rendererType, canvasId) {
     cyto.sketch = cyto.canvas.getAttribute('data-sketch');
     cyto.path   = '/sketches/'
 
+    cyto.utils           = new Utils();
     cyto.eventDispatcher = new EventDispatcher();
     cyto.view            = new View();
     cyto.errors          = new ErrorMessages();
@@ -26,7 +27,8 @@ var cyto = function(rendererType, canvasId) {
     cyto.drawEngine      = new DrawEngine();
     cyto.ellipse         = new Ellipse();
 
-    makeRootAccessible(cyto);
+
+    cyto.utils.getRootAccessProps(cyto);
 
     //load sketch
     cyto.loader.loadSketch(cyto.path + cyto.sketch, init.bind(this));
@@ -37,65 +39,10 @@ var cyto = function(rendererType, canvasId) {
       //maximize canvas view
       cyto.width   = window.innerWidth;
       cyto.height  = window.innerHeight;
+      //cyto.view.init();
 
-      cyto.drawEngine.start(cyto.canvas);
+      cyto.start();
     }
-
-    function makeRootAccessible(target) {
-      var p;
-      for(var obj in target) {
-        if(typeof(target[obj]) === 'object' && target[obj]._rootAccessible) {
-          // apply instance methods & properties onto the root
-          for(var key in target[obj]) {
-            if(target[obj].hasOwnProperty(key) && !String(key).match(/_/)) {
-              if(typeof(target[obj][key]) === 'function') {
-                target[key] = target[obj][key].bind(target[obj]);
-              } else {
-
-                /*
-                *  get or set subclass property from root
-                *
-                * important to bind target[obj] and pass key arg
-                * within anonymous function; else we would only have
-                * access to the last target[obj] in the for loop iteration
-                * also, javascript passes value by copy-ref, so we
-                * cannot simply say root[key] = root[object][key]
-                * and have values stay in sync
-                */
-
-                (function(key) {
-                  var subclass = this;
-
-                  Object.defineProperty(target, key, {
-                    get: function() {
-                      return subclass[key];
-                    },
-                    set: function(value) {
-                      subclass[key] = value;
-                    },
-                    enumerable: true
-                  });
-
-                }.bind(target[obj]))(key);
-
-              }
-              //target[key] = target[o][key];
-            }
-          }
-          //  apply the prototype properties and methods onto the root
-          p = Object.getPrototypeOf(target[obj]);
-          for(var key in p) {
-            if(p.hasOwnProperty(key) && !String(key).match(/_/)) {
-              if(typeof(p[key]) === 'function') {
-                target[key] = p[key].bind(target[obj]);
-              } else {
-                target[key] = p[key];
-              }
-            }
-          }
-        }
-      }
-    };
 
   };
 
--- cyto-lib/src/cyto/Utils.js
+++ cyto-lib/src/cyto/Utils.js
@@ -1,3 +1,61 @@
+var Utils = function() {};
+
+Utils.prototype.getRootAccessProps = function (target) {
+  var p;
+  for(var obj in target) {
+    if(typeof(target[obj]) === 'object' && target[obj]._rootAccessible) {
+      // apply instance methods & properties onto the root
+      for(var key in target[obj]) {
+        if(target[obj].hasOwnProperty(key) && !String(key).match(/_/)) {
+          if(typeof(target[obj][key]) === 'function') {
+            target[key] = target[obj][key].bind(target[obj]);
+          } else {
+
+            /*
+            *  get or set subclass property from root
+            *
+            * important to bind target[obj] and pass key arg
+            * within anonymous function; else we would only have
+            * access to the last target[obj] in the for loop iteration
+            * also, javascript passes value by copy-ref, so we
+            * cannot simply say root[key] = root[object][key]
+            * and have values stay in sync
+            */
+
+            (function(key) {
+              var subclass = this;
+
+              Object.defineProperty(target, key, {
+                get: function() {
+                  return subclass[key];
+                },
+                set: function(value) {
+                  subclass[key] = value;
+                },
+                enumerable: true
+              });
+
+            }.bind(target[obj]))(key);
+
+          }
+          //target[key] = target[o][key];
+        }
+      }
+      //  apply the prototype properties and methods onto the root
+      p = Object.getPrototypeOf(target[obj]);
+      for(var key in p) {
+        if(p.hasOwnProperty(key) && !String(key).match(/_/)) {
+          if(typeof(p[key]) === 'function') {
+            target[key] = p[key].bind(target[obj]);
+          } else {
+            target[key] = p[key];
+          }
+        }
+      }
+    }
+  }
+};
+
 // define([
 //   '/lodash/dist/lodash.js',
 //   '/cyConstants.js'
