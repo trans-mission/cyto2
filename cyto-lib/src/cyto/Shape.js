6199145db98c3a8a8958c6fe9c289727b3325982
mmansion <mikhail@tellart.com>
2015-06-08 22:43:11 -0400

    Adding shape class

---
+++ cyto-lib/src/cyto/Shape.js
@@ -0,0 +1,36 @@
+var Shape = function() {
+
+};
+
+Shape.prototype.collidesWith = function() {
+
+};
+
+Shape.prototype.separationOnAxes = function() {
+
+};
+
+Shape.prototype.move = function() {
+
+};
+
+Shap.prototype.createPath = function() {
+
+};
+
+Shape.prototype.getAxes = function() {
+
+};
+
+Shape.isPointInPath = function() {
+  
+}
+
+//   p.project = function(axis) {
+//     throw 'project(axis) not implemented';
+//   };
+//
+//   p.isPointInPath = function(context, x, y) {
+//     this.createPath(context);
+//     return context.isPointInPath(x, y);
+//   };
