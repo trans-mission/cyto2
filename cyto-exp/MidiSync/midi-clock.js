5aab440915de9c105d44428e055b2cf48dbafafb
mmansion <mikhail@tellart.com>
2015-06-13 22:31:00 -0400

    Midi sync testing

--- cyto-exp/MidiSync/midi-clock.js
+++ cyto-exp/MidiSync/midi-clock.js
@@ -1,35 +1,79 @@
-var MidiClock = require('midi-clock');
+var midi = require('midi');
+var socketio = require('socket.io');
 
-// in node just do this (uses process.hrtime):
-var clock = MidiClock();
+var MIDI_PPQN = 24
+var SOCKET_PPQN = 4;
+var MIDI_CLOCK = 248;
+var MIDI_START = 251;
+var MIDI_CONTINUE = 252;
+var clockCount = 0;
 
-// in browser
 
-var audioContext;
-if (typeof AudioContext !== "undefined") {
-  audioContext = new AudioContext();
-} else if (typeof webkitAudioContext !== "undefined") {
-  audioContext = new webkitAudioContext();
-} else {
-    throw new Error('AudioContext not supported. :(');
+// Set up a new input.
+var input = new midi.input();
+
+var devices = [];
+for (var i = 0; i < input.getPortCount(); i ++){
+    devices[i] = input.getPortName(i);
 }
 
+if (process.argv.length < 4){
+    console.log('Run the server using "node pulserver.js [device_id] [port]"');
+    console.log('    e.g. "node pulserver.js 0 9000"');
 
-var clock = MidiClock(audioContext);
+    console.log("The following MIDI devices were detected:");
+    if (devices.length == 0){
+        console.log("...NO DEVICES DETECTED...");
+    } else {
+        for (var i = 0; i < devices.length; i ++){
+            console.log( "id: " + i + "\t" + devices[i]);
+        }
+    }
+    process.exit(0);
+} else {
+    var deviceID = parseInt(process.argv[2])
+    var port = parseInt(process.argv[3])
 
-clock.start();
+    if (deviceID >= devices.length){
+        throw "Device " + deviceID + " unknown."
+    }
+    console.log("Listening to device " + deviceID + ": " + devices[deviceID])
 
-clock.on('position', function(position){
+    io = socketio.listen(port);
+    io.sockets.on('connection', function (socket) {
+        console.log("Client connected.");
 
-  // log on each beat, ignore the rest
-  var microPosition = position % 24
-  if (microPosition === 0){
-    console.log('Beat:', position / 24)
-  }
+        socket.on('disconnect', function(reason){
+            console.log("Disconnected: " + reason);
+        });
 
-});
+        socket.on('ping', function(){
+            socket.emit('pong');
+        });
+    });
 
-setTimeout(function(){
-  // change to 120bpm after 10 seconds
-  clock.setTempo(120)
-}, 10000);
+
+    var midiReceived = function(deltaTime, message){
+        // Throttle the number of clock messages sent.
+        // The midi standard of 24 pulses-per-quarter-note
+        // (e.g. 120*24 = 2880 messages per second @ 12bpm) is a
+        // little high to pump through a web socket.
+        if (message == MIDI_CLOCK){
+            if (clockCount % (MIDI_PPQN/SOCKET_PPQN) == 0){
+                io.sockets.emit('midi', message);
+                clockCount = 0;
+            }
+            clockCount ++;
+        } else {
+            io.sockets.emit('midi', message);
+            if (message == MIDI_START || message == MIDI_CONTINUE){
+                clockCount = 0;
+            }
+        }
+    }
+
+    input.openPort(deviceID);
+    input.ignoreTypes(false, false, false);
+    input.on('message', midiReceived);
+
+}
--- cyto-exp/MidiSync/node-midi.js
+++ cyto-exp/MidiSync/node-midi.js
@@ -1,17 +1,50 @@
 var midi = require('midi');
 
+
+var MIDI_PPQN = 24
+var SOCKET_PPQN = 4;
+var MIDI_CLOCK = 248;
+var MIDI_START = 251;
+var MIDI_CONTINUE = 252;
+var clockCount = 0;
+
+var VMIDI_PORT = "CYTO MIDI IN";
+
+
 // Set up a new input.
 var input = new midi.input();
 
 //Configure a callback.
-input.on('message', function(deltaTime, message) {
-    console.log('m:' + message + ' d:' + deltaTime);
-});
+// input.on('message', function(deltaTime, message) {
+//     console.log('m:' + message + ' d:' + deltaTime);
+// });
 
 
+var midiReceived = function(deltaTime, message){
+  // console.log(message);
+          console.log('m:' + message + ' d:' + deltaTime);
+  return;
+    // Throttle the number of clock messages sent.
+    // The midi standard of 24 pulses-per-quarter-note
+    // (e.g. 120*24 = 2880 messages per second @ 12bpm) is a
+    // little high to pump through a web socket.
+    if (message == MIDI_CLOCK){
+        if (clockCount % (MIDI_PPQN/SOCKET_PPQN) == 0){
+            //io.sockets.emit('midi', message);
+            clockCount = 0;
+        }
+        clockCount ++;
+    } else {
+      console.log("different message = ", message);
+        //io.sockets.emit('midi', message);
+        if (message == MIDI_START || message == MIDI_CONTINUE){
+            clockCount = 0;
+        }
+    }
+};
 
 // Create a virtual input port.
-input.openVirtualPort("Cyto MIDI-IN");
+input.openVirtualPort(VMIDI_PORT);
 
 
 // Sysex, timing, and active sensing messages are ignored
@@ -22,3 +55,5 @@ input.openVirtualPort("Cyto MIDI-IN");
 // you should use
 // input.ignoreTypes(true, false, true)
 input.ignoreTypes(false, false, false);
+
+input.on('message', midiReceived);
